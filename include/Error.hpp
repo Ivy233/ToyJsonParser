9cf0ba0983484dce46a2d1b2e472c68ce8a5ac66
Ivy233 <952254420@qq.com>
2020-03-05 21:37:04 +0800

    add tests

---
+++ include/Error.hpp
@@ -0,0 +1,56 @@
+#ifndef _ERROR_HPP_
+#define _ERROR_HPP_
+#include "json_t.hpp"
+#include <cassert>
+#include <iostream>
+namespace ToyJson
+{
+namespace Error
+{
+// Value
+void type_error(json_t _type, json_t _type1, json_t _type2)
+{
+    printf("type error: expect %s or %s rather than %s\n", _M_type[_type1], _M_type[_type2], _M_type[_type]);
+    assert(0);
+}
+void type_error(json_t _type, json_t _type1)
+{
+    printf("type error: expect %s rather than %s\n", _M_type[_type1], _M_type[_type]);
+    assert(0);
+}
+void cannot_find_error(const char *_key)
+{
+    printf("cannot find: %s in Object\n", _key);
+    assert(0);
+}
+void border_error(size_t _pos, size_t _siz)
+{
+    printf("border error: need index %lld, but size %lld\n", _pos, _siz);
+    assert(0);
+}
+// file
+void io_error(const char *_filedir)
+{
+    printf("io_error: file %s cannot open\n", _filedir);
+    assert(0);
+}
+// Parser
+void expect_error(size_t _line, const char *_thisline, const char *_meet, const char *_expect)
+{
+    printf("line %lld: expect \"%s\", but meet \"%s\" in \"%s\"\n", _line, _expect, _meet, _thisline);
+    assert(0);
+}
+void slash_error(size_t _line, const char *_thisline, const char *_meet)
+{
+    printf("line %lld: can parse \"%s\" in \"%s\", but the next char cannot be parsed\n", _line, _meet, _thisline);
+    assert(0);
+}
+void cannot_parse_error(size_t _line, const char *_thisline, const char *_meet)
+{
+    printf("line %lld: encounted chars \"%s\" in \"%s\" is not the beginning of a json structure\n", _line, _meet, _thisline);
+    assert(0);
+}
+} // namespace Error
+}; // namespace ToyJson
+
+#endif
